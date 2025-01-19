ee02fc6abe5173af015899cdcc5a72e2b93eb281
Xaphan67 <cedric.falda@gmail.com>
2025-01-19 03:55:08 +0100

    fix: Force drone grab when spawned

--- Code/Entities/Celeste/Drone.cs
+++ Code/Entities/Celeste/Drone.cs
@@ -459,7 +459,16 @@ namespace Celeste.Mod.XaphanHelper.Entities
         public override void Added(Scene scene)
         {
             base.Added(scene);
-            if (!XaphanModule.ModSettings.UseBagItemSlot.Check && !XaphanModule.ModSaveData.startAsDrone[SceneAs<Level>().Session.Area.LevelSet])
+            if (!XaphanModule.ModSaveData.startAsDrone.ContainsKey(SceneAs<Level>().Session.Area.LevelSet))
+            {
+                if (player != null && player.Holding != Hold)
+                {
+                    player.Holding = Hold;
+                    Hold.Pickup(player);
+                    player.StateMachine.State = Player.StPickup;
+                }
+            }
+            else if (!XaphanModule.ModSettings.UseBagItemSlot.Check && !XaphanModule.ModSaveData.startAsDrone[SceneAs<Level>().Session.Area.LevelSet])
             {
                 RemoveSelf();
             }
